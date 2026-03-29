6efc9e68d6af9cf8e61c1b9c11b08b514416fa53
QMaffin <fengaidan18@gmail.com>
2026-03-29 18:13:32 -0500

    Made a d20 with basic seed rng

---
+++ d20dice.py
@@ -0,0 +1,19 @@
+# This is just a basic test on how to make each dice roll random. Can be improved upon or
+# changed depending on future needs. - AF
+
+from random import seed, random
+
+# Each time this is called, it will make a new seed and use that seed to return 
+# a dice roll from 1-20. By default, the seed is generated based on the current
+# system time. 
+def roll_dice(): 
+    seed()
+    dice_roll = int(random()*20 + 1)
+    return dice_roll
+# May run into a problem if, for example, each session is played around the same 
+# time, resulting in similar rolls. Can add more random factors to increase rng. - AF
+
+# I don't exactly know how advantage rolls work with stats. Cole has to explain or 
+# integrate that himself. - AF
+
+print(roll_dice())
