2ad6d9bd29cc1a05d689d96a50617bc2dd35f34b
QuesoBlanco15 <cem.star09@gmail.com>
2026-04-29 16:12:55 -0500

    CRACKED PERSISTANT STORAGE!!!!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QSplitter, QVBoxLayout
 from diceClass import *
 from views import *
+import json, os
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -46,9 +47,14 @@ class MainWindow(QMainWindow):
 
         # Character Sheet View
         characterSheet = CharacterSheetView()
+        self.characterSheet = characterSheet
 
         # Dice Multipliers/stats view (?)
         multipliers = MultiplierView()
+        self.multipliers = multipliers
+
+        self.dice = dice
+        self._load_settings()
 
         dice.roll_completed.connect(multipliers.on_roll_completed)
 
@@ -65,7 +71,38 @@ class MainWindow(QMainWindow):
         main_view.setSizes([85, 400, 600])
 
         layout.addWidget(main_view)
-
+    
+    def closeEvent(self, event):
+        d = self.dice.dice  # DiceWidget
+        settings = {
+            "dice_type":   d.dice_type,
+            "body_color":  d.body_color.name(),
+            "edge_color":  d.edge_color.name(),
+            "num_color":   d.num_color.name(),
+            "show_triangle": d.show_triangle,
+        }
+        with open("settings.json", "w") as f:
+            json.dump(settings, f, indent=2)
+        event.accept()
+    
+    def _load_settings(self):
+        if not os.path.exists("settings.json"):
+            return
+        with open("settings.json") as f:
+            s = json.load(f)
+        d = self.dice.dice
+        if "body_color" in s:
+            color = QColor(s["body_color"])
+            d.set_body_color(color)
+            self.dice.apply_accent(color)
+            self.characterSheet.apply_accent(color)
+            self.multipliers.apply_accent(color)
+        if "edge_color"  in s: d.set_edge_color(QColor(s["edge_color"]))
+        if "num_color"   in s: d.set_num_color(QColor(s["num_color"]))
+        if "show_triangle" in s: d.set_show_triangle(s["show_triangle"])
+        if "dice_type"   in s:
+            d.set_dice(s["dice_type"])
+            self.dice.dice_select.setCurrentText(f"d{s['dice_type']}")
     
     
 
